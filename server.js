ea3fa7f79d8702dde23b766301e0c509dacc8e1a
Thorsten Zoerner <zoerner@gmail.com>
2017-04-15 11:09:10 +0000

    Changed HAPI Port to 8081 for C9

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+    host: '0.0.0.0', 
+    port: 8081 
 });
 
 
