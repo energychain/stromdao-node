9fd6345a6c4e5f8994d1b52886c3dbcc4fb1b007
Thorsten Zoerner <zoerner@gmail.com>
2017-04-16 00:48:47 +0000

    Added basic mocha test to kick start build integration

---
+++ test/oracle_discovergy.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+    
+
+describe('Bootstrap', function() {
+  describe('DGY Bootstrap', function() {
+    it('Test Bootstrap completed', function() {
+      const Bootstrap=require("../oracles/discovergy/bootstrap.js");
+      const Discovergy=require("../oracles/discovergy/discovergy.js");
+      var bootstrap = new Bootstrap(function() {
+                assert.ok(typeof bootstrap.deployment != "undefined");
+                assert.ok(typeof bootstrap.ipfs != "undefined");
+                assert.ok(typeof bootstrap.storage != "undefined");
+                assert.ok(typeof bootstrap.config != "undefined");
+                
+    		
+    });
+     
+    });
+  });
+});
