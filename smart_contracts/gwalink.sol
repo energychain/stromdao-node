7a0723874379a1bb6bce66c6e11f446b5372d704
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-04-22 16:53:33 +0200

    Added basic Business Object for Electricity Handling with a Blockchain Technology

--- smart_contracts/gwalink.sol
+++ smart_contracts/gwalink.sol
@@ -110,7 +110,6 @@ contract GWALink is owned {
     }
 }
 
-
 contract StromDAOReading is owned {
    GWALink public gwalink;
    
@@ -136,8 +135,6 @@ contract StromDAOReading is owned {
    }
 }
 
-
-
 contract PrivatePDcontract is owned {
     address public from;
     address public to;
@@ -147,7 +144,7 @@ contract PrivatePDcontract is owned {
     uint256 public cost_sum;
     address public mpid;
     PrivatePDcontract public next;
- 
+    BalancerOracles public stromkonto;
     bool public started;
     bool public endure;
     bool public executed;
@@ -163,7 +160,7 @@ contract PrivatePDcontract is owned {
         address oracle;
     }
     
-    function PrivatePDcontract(GWALink _link,address _mpid,address _from, address _to,uint256 _wh_microcent,uint256 _min_tx_microcent,bool _endure) {
+    function PrivatePDcontract(GWALink _link,BalancerOracles _stromkonto,address _mpid,address _from, address _to,uint256 _wh_microcent,uint256 _min_tx_microcent,bool _endure) {
         gwalink=_link;
         from=_from;
         to=_to;
@@ -176,6 +173,7 @@ contract PrivatePDcontract is owned {
         if(_wh_microcent>0) {
         min_wh=_min_tx_microcent/_wh_microcent;
         }
+        stromkonto=_stromkonto;
         started=false;
     }
     function init() {
@@ -192,7 +190,10 @@ contract PrivatePDcontract is owned {
         if((cur_power_in>zs_start+min_wh)&&(!executed)) {
             zs_end= cur_power_in;
             if(endure) {
-                cost_sum+=wh_microcent*(zs_end-zs_start);
+                uint256 microcent = wh_microcent*(zs_end-zs_start);
+                uint256 cent = microcent/1000; // Hier wird eine Rundung gemacht, deren Rest nicht beachtet wird
+                cost_sum+=microcent+((cent*1000)-microcent);
+                stromkonto.addTx(from,to,cent,zs_end-zs_start); 
                 init();
             }
         }
@@ -202,4 +203,247 @@ contract PrivatePDcontract is owned {
         if(!endure) throw;
         endure=false;
     }
+}
+
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
+contract StableBalance is owned {
+ 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Tx(address _to, uint256 _value,string _txt);
+    
+    mapping (address => uint256) balances;
+    
+    function transfer(address _to, uint256 _value) returns (bool success) { return false; throw;}
+    
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+    
+    function addTx(address _to, uint256 _value,string _txt) onlyOwner {
+        balances[_to]+=_value;
+        Tx(_to,_value,_txt);
+    }
+    
+}
+
+contract Stromkonto is owned {
+ 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Tx(address _from,address _to, uint256 _value,uint256 _base);
+    
+    mapping (address => uint256) balancesHaben;
+    mapping (address => uint256) balancesSoll;
+    
+    function transfer(address _to, uint256 _value) returns (bool success) { return false; throw;}
+    
+    function balanceHaben(address _owner) constant returns (uint256 balance) {
+        return balancesHaben[_owner];
+    }
+    
+    function balanceSoll(address _owner) constant returns (uint256 balance) {
+        return balancesSoll[_owner];
+    }
+    
+    function addTx(address _from,address _to, uint256 _value,uint256 _base) onlyOwner {
+        balancesSoll[_from]+=_value;
+        balancesHaben[_to]+=_value;
+        Tx(_from,_to,_value,_base);
+    }
+    
+}
+
+contract BalancerOracles is owned {
+    Stromkonto public balancer;
+    event Tx(address _from,address _to, uint256 _value,uint256 _txt);
+    
+    mapping (address => bool) public oracles;
+    
+    function BalancerOracle() {
+        oracles[msg.sender]=true;
+    }
+    
+    function setBalancer(Stromkonto _balancer) onlyOwner {
+        balancer=_balancer;
+    }
+    
+    function balanceHaben(address _owner) constant returns (uint256 balance) {
+        return balancer.balanceHaben(_owner);
+    }
+    
+    function balanceSoll(address _owner) constant returns (uint256 balance) {
+        return balancer.balanceSoll(_owner);
+    }
+    
+    function addTx(address _from,address _to, uint256 _value,uint256 _base)  {
+        if(oracles[msg.sender]==true) {
+            balancer.addTx(_from,_to,_value,_base);
+            Tx(_from,_to,_value,_base);
+        } else {
+            throw;
+        }
+    }
+    
+    function transferOracleOwnership(address _owner) onlyOwner {
+        balancer.transferOwnership(_owner);
+    }
+    
+    function addOracle(address _oracle) onlyOwner {
+        oracles[_oracle] = true;
+    }
+    
+    function removeOracle(address _oracle) onlyOwner {
+        oracles[_oracle] = false;
+    }
+}
+
+contract StableStore {
+    
+    mapping (address => string) public store;
+    
+    function setValue(string _value) {
+        store[msg.sender]=_value;
+    }
+}
+
+contract StableAddressStore {
+    mapping (address => mapping(address=>string)) public store;
+    
+    function setValue(address key,string _value) {
+        store[msg.sender][key]=_value;
+    }
+}
+
+contract StableTxStore {
+    mapping (address => mapping(address=>tx)) public store;
+    
+    struct tx {
+        uint256 amount;
+        uint256 repeatMinutes;
+        uint256 repeatTimes;
+    }
+    
+    function setValue(address key,uint256 amount,uint256 repeatMinutes,uint256 repeatTimes) {
+        store[msg.sender][key]=tx(amount,repeatMinutes,repeatTimes);
+    }
+}
+
+contract SPV {
+     /* Public variables of the token */
+    string public standard = 'Token 0.1';
+    string public name='SPV';
+    string public symbol='/';
+    uint8 public decimals=2;
+    uint256 public totalSupply;
+    address public oracle;
+    uint8 private min_muxamount=100;
+    uint24 public num_shareholders=0;
+    Stromkonto public stromkonto;
+    address public supervisor;
+    
+
+    address[] public shareholders;
+    
+    /* This creates a map with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => uint256) public bufferOf; //buffered Amount before demux
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event TXin(uint256 amount);
+    event TXout(uint256 amount,address to);
+    event SHnew(address _new);
+    event SHexist(address _new);
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    
+    function SPV(
+        uint256 initialSupply,
+        Stromkonto _stromkonto 
+        ) {
+       
+       balanceOf[msg.sender] = initialSupply;   // Give the creator all initial tokens
+
+        totalSupply = initialSupply;             // Update total supply
+        stromkonto=_stromkonto;
+        supervisor=msg.sender;
+    }
+
+    /* Send coins */
+    
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        /* Check if we already know this shareholder from past - if not add this one */
+        
+        addShareholder(_to);
+        
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+        
+    }
+    
+
+    function setOracle(address _oracle)  {
+        if(msg.sender!=supervisor) throw;
+        oracle=_oracle;
+    }
+    
+    function transferSupervisor(address _new_supervisor) {
+        if(msg.sender!=supervisor) throw;
+        supervisor=_new_supervisor;
+    }
+  
+    
+    function addTx(uint256 amount) {
+        if(msg.sender!=oracle) throw;
+        if(amount<totalSupply) throw;
+        /*
+        mapping (address => uint256) txMapping;
+        TXin(amount);
+        for(var i=0;i<shareholders.length;i++) {
+            txMapping[shareholders[i]]=balanceOf[shareholders[i]];
+        }   
+        for(var j=0;j<shareholders.length;j++) {
+            var share_amount=amount*(txMapping[shareholders[j]]/totalSupply);
+            txMapping[shareholders[j]]=0;
+            //=> Transfer the share_amount
+            if(share_amount>0) {
+                
+                bufferOf[shareholders[j]]+=share_amount;
+                if(bufferOf[shareholders[j]]>min_muxamount) {
+                    //StableBalance kto = (StableBalance) stromkonto;
+                    stromkonto.addTx(this,shareholders[j],bufferOf[shareholders[j]],'SPV');
+                    TXout(bufferOf[shareholders[j]],shareholders[j]);
+                    bufferOf[shareholders[j]]=0;
+                }
+                
+            }
+        }
+        */
+        
+    }
+
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
+    }
+    
+    function addShareholder(address _shareholder)  private {
+        bool found=false;
+        for(var i=0;i<num_shareholders;i++) {
+            if(shareholders[i]==_shareholder) found=true;
+        }
+        if(!found) {
+            shareholders[num_shareholders]=_shareholder;
+            num_shareholders++;
+            SHnew(_shareholder);
+        } else {
+            SHexist(_shareholder);
+        }
+    }
+    
 }
